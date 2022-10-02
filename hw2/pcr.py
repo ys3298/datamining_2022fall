666a15c2df8d8c54ea9e2f35b4ea793689492bd9
尚依蒙 <shangyimeng@naoimous-iMac.local>
2022-10-02 16:56:36 -0400

    pcr done

--- hw2/pcr.py
+++ hw2/pcr.py
@@ -1,27 +1,35 @@
 import numpy as np
-import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
 
-# Three steps of PCA:
-# 1. Scale the data — we don’t want some feature to be voted as “more important” due to scale differences.
-# 2. Calculate covariance matrix — square matrix giving the covariances between each pair of elements of a random vector
-# 3. Eigendecomposition
+def pca(X_train, y_train, X_test, y_test, n_components):
+    X_train = StandardScaler().fit_transform(X_train)
+    X_test = StandardScaler().fit_transform(X_test)
 
-def pca(X, y, n_components):
-    # step1
-    X_scaled = StandardScaler().fit_transform(X)
-    predictors = X_scaled.T
-    # step2
-    cov_matrix = np.cov(predictors)
-    # step3
-    values, vectors = np.linalg.eig(cov_matrix)
+    u, s, vh = np.linalg.svd(X_train)
+    Z = np.matmul(X_train, vh)
 
-    # percentage of explained variance
-    # explained_variances = []
-    # for i in range(len(values)):
-    #     explained_variances.append(values[i] / np.sum(values))
+    # regression on the training set
+    reg = LinearRegression().fit(Z[:,0:n_components], y_train)
+    # predict on the test set
+    Z_test = np.matmul(X_test, vh[:,0:n_components])
+    y_test_pred = reg.predict(Z_test)
+    # Mean Squared Error
+    mse = (np.square(y_test_pred - y_test)).mean()
 
-    # regression
-    reg = LinearRegression().fit(vectors, y)
-    # return
+    return mse
+
+
+## example data
+diabetes = datasets.load_diabetes()
+X = diabetes['data']
+y = diabetes['target'].reshape(-1,1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+output = list()
+for n in list(range(1, X.shape[1]+1)):
+    print(n)
+    mse = pca(X_train, y_train, X_test, y_test, n)
+    output.append(mse)
