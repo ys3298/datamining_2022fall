9903f37a71870d34882652db7bf606e47ca35e6a
尚依蒙 <shangyimeng@naoimous-iMac.local>
2022-10-28 20:55:51 -0400

    hw3

--- hw3/perceptron_nonStandard.py
+++ hw3/perceptron_nonStandard.py
@@ -5,23 +5,9 @@ import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.preprocessing import MinMaxScaler
 
-
-X, y = datasets.make_blobs(n_samples=150,n_features=2,
-                           centers=2,cluster_std=3.20)
+# reference: https://scikit-learn.org/stable/modules/generated/sklearn.datasets.make_blobs.html
+X, y = datasets.make_blobs(n_samples=15,n_features=2,centers=2,cluster_std=1)
 y[y==0]=-1
-
-#Plotting
-
-min_max_scaler = MinMaxScaler()
-X = min_max_scaler.fit_transform(X)
-
-# fig = plt.figure(figsize=(10,8))
-# plt.plot(X[:, 0][y == -1], X[:, 1][y == -1], 'r^')
-# plt.plot(X[:, 0][y == 1], X[:, 1][y == 1], 'bs')
-# plt.xlabel("feature 1")
-# plt.ylabel("feature 2")
-# plt.title('Random Classification Data with 2 classes')
-
 
 def pred(X_inp, beta_inp, beta0_inp):
     # beta_inp is a vector of size X_inp.shape[1]
@@ -61,5 +47,33 @@ def prediction_accuracy(estimated_beta, y_true, X_true):
     return acc,predicted
 
 
-beta_hat = perceptron_nonStandard(X, y, beta=np.ones(X.shape[1]), beta0=0, rho=2, mis_class = 1)
+beta_hat = perceptron_nonStandard(X, y, beta=np.ones(X.shape[1]), beta0=0, rho=2, mis_class = 0)
 results = prediction_accuracy(beta_hat, y, X)
+
+# visulize the results
+# reference: https://towardsdatascience.com/exploring-the-perceptron-algorithm-using-python-c1d3af53a7c7
+
+
+def plot_decision_boundary(X, theta):
+    # X --> Inputs
+    # theta --> parameters
+
+    # The Line is y=mx+c
+    # So, Equate mx+c = theta0.X0 + theta1.X1 + theta2.X2
+    # Solving we find m and c
+    x1 = [min(X[:, 0]), max(X[:, 0])]
+    m = -theta[1] / theta[2]
+    c = -theta[0] / theta[2]
+    x2 = m * x1 + c
+
+    # Plotting
+    fig = plt.figure(figsize=(10, 8))
+    plt.plot(X[:, 0][y == -1], X[:, 1][y == -1], "r^")
+    plt.plot(X[:, 0][y == 1], X[:, 1][y == 1], "bs")
+    plt.xlabel("Feature 1")
+    plt.ylabel("Feature 2")
+    plt.title('Perceptron Algorithm')
+    plt.plot(x1, x2, 'y-')
+
+
+plot_decision_boundary(X, beta_hat)
